a01426563507abfbadcec37b1fedfdb9afb48eeb
insamju300 <insamju300@gmail.com>
2023-12-26 17:50:00 +0900

    빼기 및 복수 입력 처리 구현

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -3,11 +3,45 @@ package com.ll;
 public class Calc {
   public static int run(String exp) {
 
-    String[] bits = exp.split(" \\+ ");
+    String[] bits = exp.split(" ");
+    String oper = null;
+    int curretnInt = 0;
 
-    int a = Integer.parseInt(bits[0]);
-    int b = Integer.parseInt(bits[1]);
+    for(String bit: bits){
+        if(isInt(bit)){
+          if(oper == null ){
+              curretnInt = Integer.parseInt(bit);
+          }else{
 
-    return a + b;
+            if(oper.equals("+")){
+              curretnInt += Integer.parseInt(bit);
+            }else if(oper.equals("-")){
+              curretnInt -= Integer.parseInt(bit);
+            }
+
+          }
+
+        }else{
+          oper = bit;
+        }
+    }
+
+//
+//    int a = Integer.parseInt(bits[0]);
+//    int b = Integer.parseInt(bits[1]);
+
+    return curretnInt;
+  }
+
+  public static boolean isInt(String input){
+    try{
+      Integer.parseInt(input);
+      return true;
+    }catch(Exception e){
+
+    }
+    return false;
+
+
   }
 }
